6f376ccc376086b918c70b2d391a54acdf5aa761
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-02-24 15:11:23 -0500

    [ADD] lint - prettier

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { createApp } from "vue";
-import store from "./store";
+import { createApp } from 'vue'
+import store from './store'
 
-import App from "./App.vue";
-import "./index.css";
+import App from './App.vue'
+import './index.css'
 
-createApp(App).use(store).mount("#app");
+createApp(App).use(store).mount('#app')
